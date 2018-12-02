2608c433d261bfd1a81af334f7e938d8e3108ec9
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2018-12-02 20:44:23 +0100

    Creat Addition.java

---
+++ src/Addition.java
@@ -0,0 +1,45 @@
+package com.calcul.Servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Addition
+ */
+@WebServlet("/Addition")
+public class Addition extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Addition() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.getServletContext().getRequestDispatcher("/WEB-INF/addition.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//doGet(request, response);
+		String op1 = (String) request.getParameter("op1");
+		String op2 = (String) request.getParameter("op2");
+		request.setAttribute("op1", op1);
+		request.setAttribute("op2", op2);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/addition.jsp").forward(request, response);
+	}
+
+}
